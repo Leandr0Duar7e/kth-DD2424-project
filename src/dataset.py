d907b784593a6a239fd2b45c792dbc588b089da3
Leandro <56501818+Leandr0Duar7e@users.noreply.github.com>
2025-05-18 21:48:24 +0200

    Update dataset.py
    small bug

--- src/dataset.py
+++ src/dataset.py
@@ -173,7 +173,7 @@ class OxfordPetDataset(Dataset):
         train_subset, val_subset, test_subset = random_split(
             dataset_for_splitting,
             [train_size, val_size, test_size],
-            generator=generatgit checkout mainor,
+            generator=generator,
         )
 
         # Prepare the final training dataset for the DataLoader
